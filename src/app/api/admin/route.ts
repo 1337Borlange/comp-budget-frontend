e901faaa282be7b33d76013b1c268772396f597a
Simon Palmberg <simon.palmberg@gmail.com>
2023-08-25 11:10:03 +0200

    Fixed post of forms

---
+++ src/app/api/admin/route.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from 'next/server';
+
+// export default async function GET(req: Request, res: Response) {
+//     const query = req.query;
+//     const { userId } = query;
+// }
