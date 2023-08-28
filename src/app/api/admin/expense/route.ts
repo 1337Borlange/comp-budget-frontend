7a7a48cac8b51f0e7f8d756e6913528d45f7c31a
Simon Palmberg <simon.palmberg@gmail.com>
2023-08-28 13:50:52 +0200

    Added action to delete expense

--- src/app/api/admin/expense/route.ts
+++ src/app/api/admin/expense/route.ts
@@ -1,24 +1,24 @@
-import { getServerSession } from 'next-auth';
-import { NextRequest, NextResponse } from 'next/server';
-import { authOptions } from '../../auth/[...nextauth]/route';
-import { revalidatePath } from 'next/cache';
-import { apiFetch } from '@/lib/helpers';
-import { apiUrl } from '@/lib/settings';
+// import { getServerSession } from 'next-auth';
+// import { NextRequest, NextResponse } from 'next/server';
+// import { authOptions } from '../../auth/[...nextauth]/route';
+// import { revalidatePath } from 'next/cache';
+// import { apiFetch } from '@/lib/helpers';
+// import { apiUrl } from '@/lib/settings';
 
-export async function DELETE(request: NextRequest) {
-  const session = await getServerSession(authOptions);
-  const id = request.nextUrl.searchParams.get('id');
-  const deleted = await apiFetch(
-    (session as any)?.id_token,
-    `${apiUrl}/adm/expenses?expenseId=${id}`,
-    { method: 'DELETE' }
-  );
-  if (deleted.status !== 200) {
-    throw new Error('Could not delete expense.');
-  }
-  revalidatePath('/admin');
-  return NextResponse.json({ revalidated: true, now: Date.now() });
-}
+// export async function DELETE(request: NextRequest) {
+//   const session = await getServerSession(authOptions);
+//   const id = request.nextUrl.searchParams.get('id');
+//   const deleted = await apiFetch(
+//     (session as any)?.id_token,
+//     `${apiUrl}/adm/expenses?expenseId=${id}`,
+//     { method: 'DELETE' }
+//   );
+//   if (deleted.status !== 200) {
+//     throw new Error('Could not delete expense.');
+//   }
+//   revalidatePath('/admin');
+//   return NextResponse.json({ revalidated: true, now: Date.now() });
+// }
 
 // export default async function GET(req: Request, res: Response) {
 //     const query = req.query;
