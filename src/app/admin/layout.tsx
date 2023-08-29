7da647b6a7be962d9d1d27b94d97424202dfb56c
Simon Palmberg <simon.palmberg@gmail.com>
2023-08-29 09:40:57 +0200

    Added refresh token

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,6 +3,7 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import { AdminContextProvider } from './components/AdminContext';
 import '../../styles/components/page.scss';
+import { redirect } from 'next/navigation';
 
 export default async function Layout({
   children,
@@ -10,6 +11,7 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   const session = await getServerSession(authOptions);
+  if (!session) redirect('/api/auth/signin');
   return (
     <>
       <div className="page-wrapper">
--- src/app/mybudget/layout.tsx
+++ src/app/mybudget/layout.tsx
@@ -2,6 +2,7 @@ import Header from '@/components/Header';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '../api/auth/[...nextauth]/route';
 import '../../styles/components/page.scss';
+import { redirect } from 'next/navigation';
 
 export default async function Layout({
   children,
@@ -9,6 +10,7 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   const session = await getServerSession(authOptions);
+  if (!session) redirect('/api/auth/signin');
   return (
     <>
       <div className="page-wrapper">
