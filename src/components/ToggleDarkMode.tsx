efd554edae8527474cb952a5a915f5d62d90c1e4
Jon Bennevall <jon.bennevall@1337.tech>
2023-11-28 12:49:52 +0100

    Removed warning and theme gets selected from browser theme if undefined

--- src/components/ToggleDarkMode.tsx
+++ src/components/ToggleDarkMode.tsx
@@ -1,24 +1,11 @@
-"use client";
+'use client';
 
-import { useDarkMode } from "../lib/hooks";
-import { Label } from "./FormControl/Label";
-import ToggleSwitch from "./ToggleSwitch";
-import { useEffect } from "react";
+import { useDarkMode } from '../lib/hooks';
+import { Label } from './FormControl/Label';
+import ToggleSwitch from './ToggleSwitch';
 
 const ToggleDarkMode = () => {
   const { isDarkMode, toggle } = useDarkMode();
-
-  // useEffect(() => {
-  //   const root = document.querySelector(":root");
-
-  //   if (isDarkMode) {
-  //     root?.classList?.add("dark");
-  //     root?.classList?.remove("light");
-  //   } else {
-  //     root?.classList?.remove("dark");
-  //     root?.classList?.add("light");
-  //   }
-  // }, [isDarkMode]);
 
   return (
     <div className="list-switch">
